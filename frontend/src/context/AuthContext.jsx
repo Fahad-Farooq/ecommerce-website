3920b7d940b704e32145f89fc0bec55f42e03481
Fahad <115634831+Fahad-Farooq@users.noreply.github.com>
2026-03-12 08:05:38 +0500

    Remove useNavigate from AuthContext
    Remove useNavigate hook and its usage in logout function.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 const Navigate = useNavigate();
 
@@ -19,7 +18,6 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
     setUser(null);
-    Navigate("/")
   };
 
   return (
